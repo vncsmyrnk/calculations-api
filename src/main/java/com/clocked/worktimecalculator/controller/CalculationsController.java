a911ec5d9ff5966be4f85f128a20abaf87f3fa18
Vinicius <vmayrink12@gmail.com>
2024-02-07 02:29:24 +0000

    Adding field validations

--- src/main/java/com/clocked/worktimecalculator/controller/CalculationsController.java
+++ src/main/java/com/clocked/worktimecalculator/controller/CalculationsController.java
@@ -4,19 +4,40 @@ import com.clocked.worktimecalculator.builder.DayBuilder;
 import com.clocked.worktimecalculator.entities.Calculation;
 import com.clocked.worktimecalculator.entities.Day;
 import com.clocked.worktimecalculator.requestbody.RequestDay;
+import jakarta.validation.Valid;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/calculations")
 public class CalculationsController {
   @PostMapping(value = "day")
-  public ResponseEntity<List<Calculation>> calculateDay(@RequestBody RequestDay requestDay) {
+  public ResponseEntity<List<Calculation>> calculateDay(@Valid @RequestBody RequestDay requestDay) {
     Day day =
         DayBuilder.of(
             requestDay.getDate(), requestDay.getRegisteredRecords(), requestDay.getShiftRecords());
     List<Calculation> calculations = day.calculate();
     return ResponseEntity.ok(calculations);
+  }
+
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult()
+        .getAllErrors()
+        .forEach(
+            (error) -> {
+              String fieldName = ((FieldError) error).getField();
+              String errorMessage = error.getDefaultMessage();
+              errors.put(fieldName, errorMessage);
+            });
+    return errors;
   }
 }
--- src/main/java/com/clocked/worktimecalculator/requestbody/RequestDay.java
+++ src/main/java/com/clocked/worktimecalculator/requestbody/RequestDay.java
@@ -1,17 +1,21 @@
 package com.clocked.worktimecalculator.requestbody;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public class RequestDay {
+  @NotNull(message = "Date must be informed")
   @JsonFormat(pattern = "yyyy-MM-dd")
   private LocalDate date;
 
+  @NotNull(message = "Registered records must be informed")
   @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
   private List<LocalDateTime> registeredRecords;
 
+  @NotNull(message = "Shift records must be informed")
   @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
   private List<LocalDateTime> shiftRecords;
 
